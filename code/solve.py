5c0b79f0c32faeb77506a2029cf0bd31c6e64d48
Edward Doyle <e.doyle@berkeley.edu>
2017-05-01 08:54:20 -0700

    lp v2.5

--- code/solve.py
+++ code/solve.py
@@ -6,14 +6,23 @@ from lp_solver import *
 
 datapath = '../data/'
 
-# for i in range(1, 22):
-#     prob = Problem(datapath + 'problem' + str(i) + '.in')
-#     prob.readFile()
-#     pickleDump(prob, i)
-#     prob.solve()
+@timeit
+def solve():
+    for i in list(range(1, 22))[::-1]:
+        solveOne(i)
 
-i = 1
-prob = Problem(datapath + 'problem' + str(i) + '.in')
-prob.readFile()
-pickleDump(prob, i)
-prob.solve()
+@timeit
+def solveOne(i):
+    prob = Problem(datapath + 'problem' + str(i) + '.in')
+    prob.readFile()
+    pickleDump(prob, i)
+    prob.solve()
+
+# i = 1
+# prob = Problem(datapath + 'problem' + str(i) + '.in')
+# prob.readFile()
+# pickleDump(prob, i)
+# prob.solve()
+
+if __name__ == "__main__":
+    solve()
